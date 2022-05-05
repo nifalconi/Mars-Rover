8bce2404d2a0e38c1142c92b781282b2a0954a7a
Nicolas Falconi <nifalconi@uc.cl>
2022-05-05 03:38:22 -0400

    style: linter

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
+/* eslint-disable import/no-import-module-exports */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/App";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root"),
 );
-// hot reloading. It works by replacing a module of the application 
+// hot reloading. It works by replacing a module of the application
 // during runtime with an updated one so that it’s available for instant use.
 module.hot.accept();
