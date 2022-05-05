0d4f7fec5db53565700308b8d6e9771404ef5679
Nicolas Falconi <nifalconi@uc.cl>
2022-05-05 03:56:08 -0400

    increase coverage to 100%

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-	moduleFileExtensions: ['js'],
+  moduleFileExtensions: ["js"],
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
   },
-	coverageDirectory: './coverage',
-	collectCoverageFrom: ['server/**/*.js'],
-	testRegex: '((\\.|/*.)(spec))\\.js?$',
-	testEnvironment: 'node',
-	coverageThreshold: {
-		global: {
-			branches: 5,
-			functions: 5,
-			lines: 5,
-		},
-	},
+  coverageDirectory: "./coverage",
+  collectCoverageFrom: ["src/**/*.js"],
+  testRegex: "((\\.|/*.)(spec))\\.js?$",
+  testEnvironment: "node",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  },
 };
