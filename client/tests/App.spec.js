5f67e35fafcad473963bcfece4de44cd16a5d3d5
Nicolas Falconi <nifalconi@uc.cl>
2022-05-05 05:04:14 -0400

    test: client tests

--- client/tests/App.spec.js
+++ client/tests/App.spec.js
@@ -1,51 +1,97 @@
 import React from "react";
 import { render } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-// import { act } from "react-dom/test-utils";
+import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
 import App from "../src/App";
-// import robotClient from "../src/client/robot-client";
+import robotClient from "../src/client/robot-client";
 
 describe("app: Mars Rover", () => {
   it("renders without crashing", () => {
     expect.hasAssertions();
+
+    // Arrange.
     const { getByText } = render(<App />);
+
+    // Act.
     const linkElement = getByText(/Mars Rover/i);
+
+    // Arrange.
     expect(linkElement).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line jest/no-commented-out-tests
-  // it("should return Mars Rover new position", async () => {
-  //   expect.hasAssertions();
+  it("should return Mars Rover new position", async () => {
+    expect.hasAssertions();
 
-  //   const position = { x: "1", y: "20", direction: "N" };
+    // Arrange.
+    const expectedPosition = { x: "1", y: "20", direction: "N" };
+    const expectedWidth = "10";
+    const expectedHeight = "10";
+    const expectedInstructions = "MM";
 
-  //   const clientResponse = jest
-  //     .spyOn(robotClient, "post")
-  //     .mockImplementation(() => Promise.resolve({ data: { ...position } }));
+    const clientResponse = jest
+      .spyOn(robotClient, "post")
+      .mockImplementation(() => Promise.resolve({ data: { ...expectedPosition } }));
 
-  //   const component = render(<App />);
-  //   const widthInput = component.getByTestId("width");
-  //   const heightInput = component.getByTestId("height");
-  //   const positionXInput = component.getByTestId("positionX");
-  //   const positionYInput = component.getByTestId("positionY");
-  //   const directionInput = component.getByTestId("direction");
-  //   const instructionsInput = component.getByTestId("instructions");
-  //   const submitButton = component.getByTestId("submit");
+    const component = render(<App />);
+    const widthInput = component.getByTestId("width");
+    const heightInput = component.getByTestId("height");
+    const positionXInput = component.getByTestId("positionX");
+    const positionYInput = component.getByTestId("positionY");
+    const directionInput = component.getByTestId("direction");
+    const instructionsInput = component.getByTestId("instructions");
+    const submitButton = component.getByTestId("submit");
 
-  //   await act(async () => {
-  //     await userEvent.clear(widthInput);
-  //     await userEvent.type(widthInput, "10");
-  //     await userEvent.clear(heightInput);
-  //     await userEvent.type(heightInput, "10");
-  //     await userEvent.type(positionXInput, "1");
-  //     await userEvent.type(positionYInput, "20");
-  //     await userEvent.type(directionInput, "N");
-  //     await userEvent.type(instructionsInput, "M");
-  //     await userEvent.click(submitButton);
-  //   });
-  //   // eslint-disable-next-line no-promise-executor-return
-  //   await new Promise((r) => setTimeout(r, 2000));
+    // Act.
+    await act(async () => {
+      await userEvent.clear(widthInput);
+      await userEvent.type(widthInput, expectedWidth);
+      await userEvent.clear(heightInput);
+      await userEvent.type(heightInput, expectedHeight);
+      await userEvent.type(positionXInput, "1");
+      await userEvent.type(positionYInput, "20");
+      await userEvent.type(directionInput, "N");
+      await userEvent.type(instructionsInput, "M");
+      await userEvent.click(submitButton);
+    });
 
-  //   await expect(clientResponse).toHaveBeenCalled();
-  // });
+    // Assert.
+    expect(clientResponse).toHaveBeenCalledWith("v1/move", {
+      width: expectedWidth,
+      height: expectedHeight,
+      instructions: expectedInstructions,
+      position: expectedPosition,
+    });
+  });
+  it("should return error message", async () => {
+    expect.hasAssertions();
+
+    // Arrange.
+    const expectedErrorMessage = {
+      response: {
+        data: {
+          status: "error",
+          message: "instructions should be a string of M, R, L",
+        },
+      },
+    };
+
+    jest
+      .spyOn(robotClient, "post")
+      .mockImplementation(() => Promise.reject(expectedErrorMessage));
+
+    const component = render(<App />);
+    const instructionsInput = component.getByTestId("instructions");
+    const submitButton = component.getByTestId("submit");
+
+    // Act.
+    await act(async () => {
+      await userEvent.type(instructionsInput, "K");
+      await userEvent.click(submitButton);
+    });
+
+    const linkElement = component.getByText(expectedErrorMessage.response.data.message);
+
+    // Assert.
+    expect(linkElement).toBeInTheDocument();
+  });
 });
