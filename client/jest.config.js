73b951b8ccdca5a0f251bbd13c8a9a5475861727
Nicolas Falconi <nifalconi@uc.cl>
2022-05-05 03:38:42 -0400

    test: modify the jest configuration

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,19 +1,23 @@
 module.exports = {
-	moduleFileExtensions: ['js'],
+  moduleFileExtensions: ["js"],
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
-      'jest-transform-stub',
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
   },
-	coverageDirectory: './coverage',
-	collectCoverageFrom: ['client/**/*.js'],
-	testRegex: '((\\.|/*.)(spec))\\.js?$',
-	setupFilesAfterEnv: ['./setupTests.js'],
-	testEnvironment: 'jsdom',
-	coverageThreshold: {
-		global: {
-			branches: 5,
-			functions: 5,
-			lines: 5,
-		},
-	},
+  coverageDirectory: "./coverage",
+  collectCoverageFrom: ["src/**/*.js"],
+  testRegex: "((\\.|/*.)(spec))\\.js?$",
+  setupFilesAfterEnv: ["./setupTests.js"],
+  coveragePathIgnorePatterns: [
+    "index.js",
+    "jest.config.js",
+  ],
+  testEnvironment: "jsdom",
+  coverageThreshold: {
+    global: {
+      branches: 5,
+      functions: 5,
+      lines: 5,
+    },
+  },
 };
