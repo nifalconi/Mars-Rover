54b1cc9f5b6bb2c41d24cf5e56e02a66fef52465
Nicolas Falconi <nifalconi@uc.cl>
2022-05-05 03:56:16 -0400

    feat router

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const express = require('express');
+const express = require("express");
+const { instructionsProcessorController } = require("./src/controller/instructions-processor");
+
 const app = express();
 const port = 8081;
 
@@ -8,18 +10,21 @@ const port = 8081;
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-	);
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'X-Requested-With,content-type'
-	);
-	next();
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type",
+  );
+  next();
 });
 
+// Controllers
+app.post("/v1/move", (req, res) => instructionsProcessorController(req, res));
+
 app.listen({ port }, () => {
-	console.log(`Course server running at http://localhost:${port}`);
+  console.log(`Course server running at http://localhost:${port}`);
 });
